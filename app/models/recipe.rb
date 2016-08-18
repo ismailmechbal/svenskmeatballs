b1d97c586181c61546384c2c32161a3b25169985
Ismail <ismail@unik.ma>
2016-08-19 00:16:32 +0200

    Add association btw user and recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -13,16 +13,23 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  slug        :string
+#  user_id     :integer
 #
 # Indexes
 #
-#  index_recipes_on_slug  (slug) UNIQUE
+#  index_recipes_on_slug     (slug) UNIQUE
+#  index_recipes_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_9606fce865  (user_id => users.id)
 #
 
 class Recipe < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
   after_commit :flush_cache
+  belongs_to :user
   validates :name, :description, :cook_time, :prep_time, :ingredient, :instruction, :image_url, presence: true
 
   class << self
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818214522) do
+ActiveRecord::Schema.define(version: 20160818220341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,9 @@ ActiveRecord::Schema.define(version: 20160818214522) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "slug"
+    t.integer  "user_id"
     t.index ["slug"], name: "index_recipes_on_slug", unique: true, using: :btree
+    t.index ["user_id"], name: "index_recipes_on_user_id", using: :btree
   end
 
   create_table "taggings", force: :cascade do |t|
@@ -83,4 +85,5 @@ ActiveRecord::Schema.define(version: 20160818214522) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "recipes", "users"
 end
