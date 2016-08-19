7d2b54e9768c2c67bd8fce45dff67600d795fc90
Ismail <ismail@unik.ma>
2016-08-19 09:51:30 +0200

    Update views to include bootstrap semantic and add navbar partial

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+  #before_filter :authenticate_user!, only: [:edit, :update, :destroy]
 
   def index
     #@recipes = Recipe.all.order(created_at: :desc)
